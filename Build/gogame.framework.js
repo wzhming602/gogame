b003c329049b78da66df11cc73f12ce83d505026
wzhming602 <wziming602@gmail.com>
2022-02-12 18:56:25 +0800

    fix add multitouch

--- Build/gogame.framework.js
+++ Build/gogame.framework.js
@@ -1166,26 +1166,26 @@ var tempDouble;
 var tempI64;
 
 var ASM_CONSTS = {
- 3302760: function() {
+ 3302856: function() {
   Module["emscripten_get_now_backup"] = performance.now;
  },
- 3302815: function($0) {
+ 3302911: function($0) {
   performance.now = function() {
    return $0;
   };
  },
- 3302863: function($0) {
+ 3302959: function($0) {
   performance.now = function() {
    return $0;
   };
  },
- 3302911: function() {
+ 3303007: function() {
   performance.now = Module["emscripten_get_now_backup"];
  },
- 3302966: function() {
+ 3303062: function() {
   return Module.webglContextAttributes.premultipliedAlpha;
  },
- 3303027: function() {
+ 3303123: function() {
   return Module.webglContextAttributes.preserveDrawingBuffer;
  }
 };
